61208cdaf9f269b06f1aa326168f13c8e0624ada
masa <masatoshi.sboard@gmail.com>
2020-09-15 21:30:50 +0900

    Done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,20 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        _n: usize,
+        s: Chars,
     }
+    let res = 1_000_000_007;
+    let mut ans = 1;
+    let mut count = vec![0; 26];
+    for &c in &s {
+        count[c as usize - 'a' as usize] += 1;
+    }
+    for i in 0..26 {
+        if count[i] != 0 {
+            ans = ans * (count[i] + 1) % res as u64;
+        }
+    }
+    ans -= 1;
+    println!("{}", ans);
 }
